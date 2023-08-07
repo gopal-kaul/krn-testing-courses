2c1c6bdf8e2b0e061c938084b75ffc5b9cdf1b36
Gopal Kaul <gopalkaul5@gmail.com>
2023-08-08 00:06:43 +0530

    Add color theme and animation and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "rgb(239 68 68)",
+            },
+            animation: {
+                fadeIn: "fadeIn 0.7s ease-in-out forwards",
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: 0, transform: "translateY(20px)" },
+                    "100%": { opacity: 1, transform: "translateY(0px)" },
+                },
+            },
+        },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animation-delay")],
 };
